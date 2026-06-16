e720e18a1f082e548a6821ffe64975f1a31a21b8
AMORADI3 <amoradi3@jaguarlandrover.com>
2026-06-16 11:05:00 +0100

    Fixed the mess that was the folder structure. decided on 3 python files: databse, controllers and master. Put some template files in all to show good MVP

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import (Flask, render_template, request)
 app=Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+
+def home():
+    return render_template("home.html")
+
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
